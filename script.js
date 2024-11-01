bb96d3e7241444648da89b1f90fe3d383190e5aa
PeterBui-BVA <clgod21@gmail.com>
2024-11-01 14:21:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth Scroll to Sections
+document.querySelectorAll("nav a").forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute("href").substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
+    });
+});
